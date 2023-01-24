1d6df4f0a361a02da681e28dd5d5f8517331d0af
sumitbera <sumit.bera@live.in>
2023-01-24 14:23:16 +1100

    Updated shell configuration

--- jmeter/runJmeter.sh
+++ jmeter/runJmeter.sh
@@ -1,15 +1,12 @@
-jmeter -n \
+./jmeter -n \                                                                                127 ↵  2164  14:16:39
     -t ./script.jmx \
-    -j ./UserServices.log \
-    -l ./UserServices.xml \
+    -j ./jmeter.log \
+    -l ./script.xml \
     -Jjmeter.save.saveservice.output_format=xml \
     -Jjmeter.save.saveservice.response_data=true \
     -Jjmeter.save.saveservice.samplerData=true \
-    -JnumberOfThreads=100 \
-    -JrampUpPeriod=1 \
-    -Jhost=gorest.co.in \
-    -Jport=443 && \
+    -JnumberOfThreads=1 && \
     echo -e "\n\n===== TEST LOGS =====\n\n" && \
-    cat UserServices.log && \
+    cat jmeter.log && \
     echo -e "\n\n===== TEST RESULTS =====\n\n" && \
-    UserServices.xml
+    script.xml
