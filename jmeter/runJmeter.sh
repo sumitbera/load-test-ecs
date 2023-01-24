fe138079091ad3f3cc639ecef92d0e70eab06f09
sumitbera <sumit.bera@live.in>
2023-01-24 13:02:51 +1100

    Updated host url and script file

--- jmeter/runJmeter.sh
+++ jmeter/runJmeter.sh
@@ -1,5 +1,5 @@
 jmeter -n \
-    -t ./UserServices.jmx \
+    -t ./script.jmx \
     -j ./UserServices.log \
     -l ./UserServices.xml \
     -Jjmeter.save.saveservice.output_format=xml \
@@ -7,7 +7,7 @@ jmeter -n \
     -Jjmeter.save.saveservice.samplerData=true \
     -JnumberOfThreads=100 \
     -JrampUpPeriod=1 \
-    -Jhost=api-alpha.trevo.my \
+    -Jhost=gorest.co.in \
     -Jport=443 && \
     echo -e "\n\n===== TEST LOGS =====\n\n" && \
     cat UserServices.log && \
